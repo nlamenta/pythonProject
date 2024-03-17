14e0fab97d667771794542e372a3fc407be9535a
Natalia Lamenta <83751@student.ue.poznan.pl>
2024-03-17 13:41:50 +0100

    dodanie polecenia

---
+++ zajecia1/zadanie3.py
@@ -0,0 +1,7 @@
+# Przygotuj listę składającą się z cyfr Twojego NIU, bez powtórzeń (np. dla NIU - 87655 powinna to być lista - [8,7,6,5]).
+
+# Następnie zapytaj użytkownika o liczbę i poinformuj go czy:
+
+# a) suma którychkolwiek dwóch cyfr z listy da nam wprowadzoną wartość
+
+# b) iloczyn którychkolwiek dwóch cyfr z listy da nam wprowadzoną wartość
