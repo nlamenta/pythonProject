b69285f360c0b798c8a8006bdd7040f78f23ac93
Natalia Lamenta <83751@student.ue.poznan.pl>
2024-04-10 11:30:12 +0200

    dodanie kredyty

--- zajecia5/kredyty.py
+++ zajecia5/kredyty.py
@@ -21,24 +21,36 @@
 # Program powinien też odpowiednio informować użytkownika jeśli dla wprowadzonych wartości
 # niemożliwe jest wygenerowanie listy rat (informując, która z wprowadzonych wartości jest problematyczna).
 
-def kredyt(kwota_kredytu, liczba_lat, procent, typ_raty):
+def kredyt(kwota_kredytu, liczba_lat, procent_roczny, typ_raty):
+    suma_odsetek = 0
+    rata_list = []
+
+    if typ_raty == 's':
+        liczba_rat = liczba_lat * 12
+        miesięczna_stopa_procentowa = procent_roczny / 100 / 12
+        rata = (kwota_kredytu * miesięczna_stopa_procentowa) / (1 - (1 + miesięczna_stopa_procentowa) ** -liczba_rat)
+        kapitał_pozostały = kwota_kredytu
 
 
+        for i in range(1, liczba_rat + 1):
+            odsetki = kapitał_pozostały * miesięczna_stopa_procentowa
+            kapitał_pozostały -= rata - odsetki
+            suma_odsetek += odsetki
+            rata_list.append((i, rata - odsetki, odsetki, rata, kapitał_pozostały))
 
-    return kwota_kredytu, liczba_lat, procent, typ_raty
 
-wejscie1 = input("Podaj kwotę kredytu: ")
-wejscie2 = input("Podaj liczbę lat: ")
-wejscie3 = input("Podaj procent w skali roku: ")
-wejscie4 = input("Podaj typ raty: ")
+    return kwota_kredytu, liczba_lat, procent_roczny, typ_raty, suma_odsetek, rata_list
+
+wejscie1 = int(input("Podaj kwotę kredytu: "))
+wejscie2 = int(input("Podaj liczbę lat: "))
+wejscie3 = float(input("Podaj procent w skali roku: "))
+wejscie4 = input("Podaj typ raty, jeśli chcesz wybrać raty malejące wpisz 'm', jeśli chcesz wybrać raty stałe wpisz 's': ")
 
 wynik = kredyt(wejscie1, wejscie2, wejscie3, wejscie4)
 
 print(f"Poniżej podane są parametry, które zostały wprowadzone do systemu. Sprawdź czy są poprawne:"
       f" \n1. Kwota kredytu {wynik[0]} \n2. Liczba lat {wynik[1]} \n3. Procent w skali roku: {wynik[2]} \n4. Typ raty: {wynik[3]}")
 
-#
-##
-###
-##
-#
+print(f"Podsumowanie twojego kredytu:"
+      f"\n1. Kwota kredytu {wynik[0]} \n2. Liczba lat {wynik[1]} \n3. Procent w skali roku: {wynik[2]} \n4. Typ raty: {wynik[3]}"
+      f"\n5. Całkowity koszt kredytu (suma odsetek): {wynik[4]} \n")
